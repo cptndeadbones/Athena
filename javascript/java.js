22eb7f1ef2bc0c2fe2ecc0a6cf59009bd0ba5517
cptndeadbones <pro14001@byui.edu>
2023-01-09 13:10:55 -0500

    java set up for todo list

---
+++ javascript/java.js
@@ -0,0 +1,15 @@
+//Variable setup
+const todoForm = document.querySelector('todo_form');
+const todoList = documnet.querySelector('todo_list');
+
+todoForm.addEventListener('submit', event => {
+    event.preventDefault(); // prevent the form from refreshing the page
+    const todoInput = document.querySelector('#todo-input');
+    const todoText = todoInput.value; // get the text from the input field
+    todoInput.value = ''; // clear the input field
+
+    // create a new list item and add it to the todo list
+    const todoItem = document.createElement('li');
+    todoItem.textContent = todoText;
+    todoList.appendChild(todoItem);
+  });
