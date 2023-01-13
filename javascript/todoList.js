eb44133d4a41d38e7dcfafb3f89a447bf508c36e
cptndeadbones <pro14001@byui.edu>
2023-01-13 11:53:19 -0500

    java patch

--- javascript/todoList.js
+++ javascript/todoList.js
@@ -4,7 +4,7 @@ const todoList = documnet.querySelector('todo_list');
 
 todoForm.addEventListener('submit', event => {
     event.preventDefault(); // prevent the form from refreshing the page
-    const todoInput = document.querySelector('#todo-input');
+    const todoInput = document.querySelector('#list_item');
     const todoText = todoInput.value; // get the text from the input field
     todoInput.value = ''; // clear the input field
 
